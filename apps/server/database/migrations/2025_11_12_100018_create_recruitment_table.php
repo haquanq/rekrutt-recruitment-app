5c9d3046a6df965cd3527de574685ca6e95090ed
Ha Quanq <quangha0120010@gmail.com>
2025-11-16 04:18:00 +0700

    Server: update Recruitment database schema

--- apps/server/database/migrations/2025_11_12_100018_create_recruitment_table.php
+++ apps/server/database/migrations/2025_11_12_100018_create_recruitment_table.php
@@ -23,28 +23,18 @@ return new class extends Migration {
             $table->timestampTZ("completed_at")->nullable();
             $table->timestampsTZ();
 
-            $table
-                ->enum("status", RecruitmentStatus::cases())
-                ->default(RecruitmentStatus::DRAFT);
+            $table->enum("status", RecruitmentStatus::cases())->default(RecruitmentStatus::DRAFT);
 
-            $table
-                ->foreignId("user_id")
-                ->constrained(table: "user", indexName: "fk_recruitment__user");
-            $table
-                ->foreignId("proposal_id")
-                ->constrained(
-                    table: "proposal",
-                    indexName: "fk_recruitment__proposal",
-                );
+            $table->foreignId("created_by_user_id")->constrained("user", "id", "fk_recruitment__creator");
+            $table->foreignId("closed_by_user_id")->constrained("user", "id", "fk_recruitment__closer");
+            $table->foreignId("proposal_id")->constrained("proposal", "id", "fk_recruitment__proposal");
         });
 
-        DB::statement(
-            "ALTER TABLE public.recruitment ADD CONSTRAINT pk_recruitment PRIMARY KEY (id)",
-        );
+        DB::statement("ALTER TABLE public.recruitment ADD CONSTRAINT pk_recruitment PRIMARY KEY (id)");
     }
 
     public function down(): void
     {
-        Schema::dropIfExists("recruitments");
+        Schema::dropIfExists("recruitment");
     }
 };
