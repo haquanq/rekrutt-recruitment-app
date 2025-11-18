6ca26ad7f18f814bb3339ed47c6443d71d5b0a3f
Ha Quanq <quangha0120010@gmail.com>
2025-11-18 15:57:07 +0700

    Server: register Auth router

--- apps/server/routes/api.php
+++ apps/server/routes/api.php
@@ -1,6 +1,17 @@
 <?php
 
+use App\Modules\Auth\Controllers\AuthController;
 use App\Modules\Auth\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
+Route::group(["prefix" => "auth", "middleware" => "protected"], function () {
+    Route::post("login", [AuthController::class, "login"])
+        ->name("login")
+        ->withoutMiddleware("protected");
+
+    Route::post("logout", [AuthController::class, "logout"]);
+    Route::post("refresh", [AuthController::class, "refresh"]);
+    Route::post("me", [AuthController::class, "me"]);
+});
 
 Route::apiResource("users", UserController::class)->middleware("protected");
