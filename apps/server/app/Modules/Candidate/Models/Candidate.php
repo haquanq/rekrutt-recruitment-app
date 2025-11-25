ec0899f8cf9c0d9057165e823b555cb0b3e2cbb1
Ha Quanq <quangha0120010@gmail.com>
2025-11-26 02:34:36 +0700

    Server: update Candidate model

--- apps/server/app/Modules/Candidate/Models/Candidate.php
+++ apps/server/app/Modules/Candidate/Models/Candidate.php
@@ -4,25 +4,31 @@ namespace App\Modules\Candidate\Models;
 
 use App\Abstracts\BaseModel;
 use App\Modules\HiringSource\Models\HiringSource;
+use App\Modules\Recruitment\Models\RecruitmentApplication;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Candidate extends BaseModel
 {
-    protected $fillable = ["firstName", "lastName", "dateOfBirth", "address", "email", "phoneNumber", "hiringSourceId"];
+    protected $guarded = ["id", "created_at", "updated_at"];
+
+    public function source(): BelongsTo
+    {
+        return $this->belongsTo(HiringSource::class);
+    }
 
     public function experiences(): HasMany
     {
         return $this->hasMany(CandidateExperience::class);
     }
 
-    public function hiringSource(): BelongsTo
-    {
-        return $this->belongsTo(HiringSource::class);
-    }
-
     public function documents(): HasMany
     {
         return $this->hasMany(CandidateDocument::class);
+    }
+
+    public function applications(): HasMany
+    {
+        return $this->hasMany(RecruitmentApplication::class);
     }
 }
