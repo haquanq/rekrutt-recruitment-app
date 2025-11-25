9925c9df3130e4b71a47594e200d9a3d413965e3
Ha Quanq <quangha0120010@gmail.com>
2025-11-25 23:35:35 +0700

    Server: update Department auth policies, namespace

--- apps/server/app/Modules/Department/Policies/DepartmentPolicy.php
+++ apps/server/app/Modules/Department/Policies/DepartmentPolicy.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Modules\Auth\Policies;
+namespace App\Modules\Department\Policies;
 
 use App\Modules\Auth\Enums\UserRole;
 use App\Modules\Auth\Models\User;
@@ -9,14 +9,12 @@ class DepartmentPolicy
 {
     public function findAll(User $user): bool
     {
-        $role = UserRole::tryFrom($user["role"]);
-        return \in_array($role, [UserRole::HIRING_MANAGER, UserRole::RECRUITER]);
+        return true;
     }
 
     public function findById(User $user): bool
     {
-        $role = UserRole::tryFrom($user["role"]);
-        return \in_array($role, [UserRole::HIRING_MANAGER, UserRole::RECRUITER]);
+        return true;
     }
 
     public function create(User $user): bool
