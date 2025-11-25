099a69d7dc4c99767918a45cc3b5e5778053a0f5
Ha Quanq <quangha0120010@gmail.com>
2025-11-26 04:08:41 +0700

    Server: refactor route bindings

--- apps/server/routes/api.php
+++ apps/server/routes/api.php
@@ -10,110 +10,30 @@ use App\Modules\HiringSource\Controllers\HiringSourceController;
 use App\Modules\Position\Controllers\PositionController;
 use App\Modules\RatingScale\Controllers\RatingScaleController;
 use App\Modules\RatingScale\Controllers\RatingScalePointController;
-use App\Modules\RatingScale\Models\RatingScale;
 use Illuminate\Support\Facades\Route;
 
-Route::group(["prefix" => "auth", "middleware" => "protected"], function () {
-    Route::post("login", [AuthController::class, "login"])
-        ->name("login")
-        ->withoutMiddleware("protected");
+Route::prefix("auth")->group(function () {
+    Route::post("login", [AuthController::class, "login"])->name("login");
+    Route::post("refresh", [AuthController::class, "refresh"]);
 
-    Route::post("refresh", [AuthController::class, "refresh"])
-        ->name("refresh")
-        ->withoutMiddleware("protected");
-
-    Route::post("logout", [AuthController::class, "logout"]);
-    Route::post("me", [AuthController::class, "me"]);
+    Route::middleware(["protected"])->group(function () {
+        Route::post("logout", [AuthController::class, "logout"]);
+        Route::post("me", [AuthController::class, "me"]);
+    });
 });
 
 Route::middleware("protected")->group(function () {
-    Route::controller(UserController::class)
-        ->prefix("users")
-        ->group(function () {
-            Route::get("/", "index");
-            Route::get("/{id}", "show");
-            Route::post("/", "store");
-            Route::put("/{id}", "update");
-            Route::delete("/{id}", "destroy");
-            Route::patch("/{id}/status", "updateStatus");
-        });
-
-    Route::controller(DepartmentController::class)
-        ->prefix("departments")
-        ->group(function () {
-            Route::get("/", "index");
-            Route::get("/{id}", "show");
-            Route::post("/", "store");
-            Route::put("/{id}", "update");
-            Route::delete("/{id}", "destroy");
-        });
-
-    Route::controller(PositionController::class)
-        ->prefix("positions")
-        ->group(function () {
-            Route::get("/", "index");
-            Route::get("/{id}", "show");
-            Route::post("/", "store");
-            Route::put("/{id}", "update");
-            Route::delete("/{id}", "destroy");
-        });
-
-    Route::controller(RatingScaleController::class)
-        ->prefix("rating-scales")
-        ->group(function () {
-            Route::get("/", "index");
-            Route::get("/{id}", "show");
-            Route::post("/", "store");
-            Route::put("/{id}", "update");
-            Route::delete("/{id}", "destroy");
-        });
-
-    Route::controller(RatingScalePointController::class)
-        ->prefix("rating-scale-points")
-        ->group(function () {
-            Route::get("/{id}", "show");
-            Route::post("/", "store");
-            Route::put("/{id}", "update");
-            Route::delete("/{id}", "destroy");
-        });
-
-    Route::controller(ContractTypeController::class)
-        ->prefix("contract-types")
-        ->group(function () {
-            Route::get("/", "index");
-            Route::get("/{id}", "show");
-            Route::post("/", "store");
-            Route::put("/{id}", "update");
-            Route::delete("/{id}", "destroy");
-        });
-
-    Route::controller(EducationLevelController::class)
-        ->prefix("education-levels")
-        ->group(function () {
-            Route::get("/", "index");
-            Route::get("/{id}", "show");
-            Route::post("/", "store");
-            Route::put("/{id}", "update");
-            Route::delete("/{id}", "destroy");
-        });
-
-    Route::controller(ExperienceLevelController::class)
-        ->prefix("experience-levels")
-        ->group(function () {
-            Route::get("/", "index");
-            Route::get("/{id}", "show");
-            Route::post("/", "store");
-            Route::put("/{id}", "update");
-            Route::delete("/{id}", "destroy");
-        });
-
-    Route::controller(HiringSourceController::class)
-        ->prefix("hiring-sources")
-        ->group(function () {
-            Route::get("/", "index");
-            Route::get("/{id}", "show");
-            Route::post("/", "store");
-            Route::put("/{id}", "update");
-            Route::delete("/{id}", "destroy");
-        });
+    Route::apiResource("users", UserController::class)->parameters(["users" => "id"]);
+    Route::apiResource("departments", DepartmentController::class)->parameters(["departments" => "id"]);
+    Route::apiResource("positions", PositionController::class)->parameters(["positions" => "id"]);
+    Route::apiResource("rating-scales", RatingScaleController::class)->parameters(["rating-scales" => "id"]);
+    Route::apiResource("rating-scale-points", RatingScalePointController::class)->parameters([
+        "rating-scale-points" => "id",
+    ]);
+    Route::apiResource("contract-types", ContractTypeController::class)->parameters(["contract-types" => "id"]);
+    Route::apiResource("education-levels", EducationLevelController::class)->parameters(["education-levels" => "id"]);
+    Route::apiResource("experience-levels", ExperienceLevelController::class)->parameters([
+        "experience-levels" => "id",
+    ]);
+    Route::apiResource("hiring-sources", HiringSourceController::class)->parameters(["hiring-sources" => "id"]);
 });
