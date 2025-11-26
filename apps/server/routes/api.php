be99edcfe74a3bc7dad37b31975b612059542cd6
Ha Quanq <quangha0120010@gmail.com>
2025-11-26 22:37:46 +0700

    Server: add Candidate routes

--- apps/server/routes/api.php
+++ apps/server/routes/api.php
@@ -2,6 +2,9 @@
 
 use App\Modules\Auth\Controllers\AuthController;
 use App\Modules\Auth\Controllers\UserController;
+use App\Modules\Candidate\Controllers\CandidateController;
+use App\Modules\Candidate\Controllers\CandidateDocumentController;
+use App\Modules\Candidate\Controllers\CandidateExperienceController;
 use App\Modules\ContractType\Controllers\ContractTypeController;
 use App\Modules\Department\Controllers\DepartmentController;
 use App\Modules\EducationLevel\Controllers\EducationLevelController;
@@ -24,16 +27,28 @@ Route::prefix("auth")->group(function () {
 
 Route::middleware("protected")->group(function () {
     Route::apiResource("users", UserController::class)->parameters(["users" => "id"]);
+
     Route::apiResource("departments", DepartmentController::class)->parameters(["departments" => "id"]);
     Route::apiResource("positions", PositionController::class)->parameters(["positions" => "id"]);
+
     Route::apiResource("rating-scales", RatingScaleController::class)->parameters(["rating-scales" => "id"]);
     Route::apiResource("rating-scale-points", RatingScalePointController::class)->parameters([
         "rating-scale-points" => "id",
     ]);
+
     Route::apiResource("contract-types", ContractTypeController::class)->parameters(["contract-types" => "id"]);
     Route::apiResource("education-levels", EducationLevelController::class)->parameters(["education-levels" => "id"]);
     Route::apiResource("experience-levels", ExperienceLevelController::class)->parameters([
         "experience-levels" => "id",
     ]);
+
     Route::apiResource("hiring-sources", HiringSourceController::class)->parameters(["hiring-sources" => "id"]);
+
+    Route::apiResource("candidates", CandidateController::class)->parameters(["candidates" => "id"]);
+    Route::apiResource("candidate-documents", CandidateDocumentController::class)->parameters([
+        "candidate-documents" => "id",
+    ]);
+    Route::apiResource("candidate-experiences", CandidateExperienceController::class)->parameters([
+        "candidate-experiences" => "id",
+    ]);
 });
