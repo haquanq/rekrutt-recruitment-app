bedc18ac0b44f62d9272c1a5de303e0204423ec3
Ha Quanq <quangha0120010@gmail.com>
2025-11-29 23:59:06 +0700

    Server: add Proposal creation rule

--- apps/server/app/Modules/Proposal/Requests/StoreProposalRequest.php
+++ apps/server/app/Modules/Proposal/Requests/StoreProposalRequest.php
@@ -4,6 +4,7 @@ namespace App\Modules\Proposal\Requests;
 
 use App\Modules\Proposal\Abstracts\BaseProposalRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class StoreProposalRequest extends BaseProposalRequest
 {
@@ -14,7 +15,20 @@ class StoreProposalRequest extends BaseProposalRequest
 
     public function rules(): array
     {
-        return array_merge(parent::rules(), []);
+        return array_merge(parent::rules(), [
+            "position_id" => [
+                "required",
+                "integer",
+                Rule::exists("position", "id")->where("department_id", Auth::user()->position->department->id),
+            ],
+        ]);
+    }
+
+    public function messages(): array
+    {
+        return [
+            "position_id.exists" => "Position id must be in the same department as the current user",
+        ];
     }
 
     public function prepareForValidation(): void
