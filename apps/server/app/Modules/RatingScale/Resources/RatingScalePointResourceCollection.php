eacc0f4ccdb827673138cc17dd224afab033c548
Ha Quanq <quangha0120010@gmail.com>
2025-12-02 00:17:25 +0700

    Server: add RatingScalePointResourceCollection

---
+++ apps/server/app/Modules/RatingScale/Resources/RatingScalePointResourceCollection.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Modules\RatingScale\Resources;
+
+use App\Abstracts\BaseResourceCollection;
+
+class RatingScalePointResourceCollection extends BaseResourceCollection
+{
+    public $collects = RatingScalePointResource::class;
+}
