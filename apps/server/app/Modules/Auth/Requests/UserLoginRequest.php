71f8093444e22cb9d7c6d5f5932cb7b1d774ff14
Ha Quanq <quangha0120010@gmail.com>
2025-12-01 20:22:18 +0700

    Server: add User login request docs

--- apps/server/app/Modules/Auth/Requests/UserLoginRequest.php
+++ apps/server/app/Modules/Auth/Requests/UserLoginRequest.php
@@ -14,7 +14,15 @@ class UserLoginRequest extends BaseFormRequest
     public function rules(): array
     {
         return [
+            /**
+             * Email address
+             * @example admin@gmail.com
+             */
             "email" => ["required", "email"],
+            /**
+             * Passowrd (default: 12345678)
+             * @example 12345678
+             */
             "password" => ["required", "string", "max:30"],
         ];
     }
