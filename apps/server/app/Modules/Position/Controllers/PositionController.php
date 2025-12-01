cd18217013fd511f53e5198a2ec499ea0099ab04
Ha Quanq <quangha0120010@gmail.com>
2025-12-01 17:53:18 +0700

    Server: add Position controller docs

--- apps/server/app/Modules/Position/Controllers/PositionController.php
+++ apps/server/app/Modules/Position/Controllers/PositionController.php
@@ -3,13 +3,11 @@
 namespace App\Modules\Position\Controllers;
 
 use App\Abstracts\BaseController;
-use App\Modules\Position\Requests\PositionIndexRequest;
 use App\Modules\Position\Requests\PositionStoreReqeust;
 use App\Modules\Position\Requests\PositionUpdateRequest;
 use App\Modules\Position\Models\Position;
 use App\Modules\Position\Resources\PositionResource;
 use App\Modules\Position\Resources\PositionResourceCollection;
-use Dedoc\Scramble\Attributes\Endpoint;
 use Dedoc\Scramble\Attributes\QueryParameter;
 use Illuminate\Support\Facades\Gate;
 use Spatie\QueryBuilder\AllowedFilter;
@@ -17,29 +15,75 @@ use Spatie\QueryBuilder\QueryBuilder;
 
 class PositionController extends BaseController
 {
+    /**
+     * Find all positions
+     *
+     * Retrive a list of positions. Allows pagination, relations and filter query.
+     */
+    #[
+        QueryParameter(
+            name: "page[number]",
+            type: "integer",
+            description: "Current page number (default: 1)",
+            example: 1,
+        ),
+    ]
+    #[
+        QueryParameter(
+            name: "page[size]",
+            type: "integer",
+            description: "Size of current page (default: 15, max: 100)",
+            example: 15,
+        ),
+    ]
+    #[
+        QueryParameter(
+            name: "include",
+            type: "string",
+            description: "Include nested relations </br>" .
+                " Allow relations: department </br>" .
+                "Example: include=department",
+        ),
+    ]
+    #[
+        QueryParameter(
+            name: "filter[*]",
+            type: "string",
+            description: "Filter by fields </br>" .
+                "Allow fields: title, departmentId </br>" .
+                "Example: filter[title]=Software Engineer",
+        ),
+    ]
     public function index()
     {
         Gate::authorize("findAll", Position::class);
 
         $positions = QueryBuilder::for(Position::class)
             ->allowedIncludes(["department"])
+            // awdawdawd
             ->allowedFilters([AllowedFilter::partial("title"), AllowedFilter::exact("departmentId", "department_id")])
             ->autoPaginate();
 
         return PositionResourceCollection::make($positions);
     }
 
+    /**
+     * Find position by Id
+     *
+     * Return a unique position
+     */
     public function show(int $id)
     {
         Gate::authorize("findById", Position::class);
-
-        $position = QueryBuilder::for(Position::class)
-            ->allowedIncludes(["department"])
-            ->findOrFail($id);
-
+        $position = Position::findOrFail($id);
         return PositionResource::make($position);
     }
 
+    /**
+     * Create position
+     *
+     * Return created position
+     */
     public function store(PositionStoreReqeust $request)
     {
         Gate::authorize("create", Position::class);
@@ -47,6 +91,11 @@ class PositionController extends BaseController
         return $this->createdResponse(new PositionResource($createdPosition));
     }
 
+    /**
+     * Update position
+     *
+     * Update position information. Return empty
+     */
     public function update(PositionUpdateRequest $request, int $id)
     {
         Gate::authorize("update", Position::class);
@@ -54,6 +103,11 @@ class PositionController extends BaseController
         return $this->noContentResponse();
     }
 
+    /**
+     * Delete position by Id
+     *
+     * Permanently delete position. Return empty
+     */
     public function destroy(int $id)
     {
         Gate::authorize("delete", Position::class);
