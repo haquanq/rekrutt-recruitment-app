e5e535c7194fb728eefe3779ea294aacfebf808d
Ha Quanq <quangha0120010@gmail.com>
2025-12-01 17:53:05 +0700

    Server: add PositionRequest docs

--- apps/server/app/Modules/Position/Abstracts/BasePositionRequest.php
+++ apps/server/app/Modules/Position/Abstracts/BasePositionRequest.php
@@ -13,9 +13,25 @@ abstract class BasePositionRequest extends BaseFormRequest
 
     public function rules(): array
     {
+        $id = \intval($this->route("id"));
+
         return [
-            "title" => ["required", "string", "max:100"],
+            /**
+             * Position title (must be unique)
+             * @example Data Analyst
+             */
+            "title" => ["required", "string", "max:100", "unique:position,title,$id"],
+
+            /**
+             * Position description
+             * @example Collects, cleans, and interprets data to identify trends and patterns that help organizations make better business decisions
+             */
             "description" => ["string", "max:500"],
+
+            /**
+             * Department Id where position belongs
+             * @example 1
+             */
             "department_id" => ["required", "integer", "exists:department,id"],
         ];
     }
