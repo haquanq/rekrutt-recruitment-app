a267284aab0fc4633a720e33fb6b4b22389ac9ae
Ha Quanq <quangha0120010@gmail.com>
2025-12-01 23:56:29 +0700

    Server: add ExperienceLevelResourceCollection

---
+++ apps/server/app/Modules/ExperienceLevel/Resources/ExperienceLevelResourceCollection.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Modules\ExperienceLevel\Resources;
+
+use App\Abstracts\BaseResourceCollection;
+
+class ExperienceLevelResourceCollection extends BaseResourceCollection
+{
+    public $collects = ExperienceLevelResource::class;
+}
