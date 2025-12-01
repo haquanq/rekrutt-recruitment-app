e3bffe9af03e06e9a03cc746bf4149071c4f0192
Ha Quanq <quangha0120010@gmail.com>
2025-12-01 21:39:26 +0700

    Server: update CandidateStatus enum

--- apps/server/app/Modules/Candidate/Enums/CandidateStatus.php
+++ apps/server/app/Modules/Candidate/Enums/CandidateStatus.php
@@ -5,7 +5,7 @@ namespace App\Modules\Candidate\Enums;
 enum CandidateStatus: string
 {
     case NEW = "NEW";
-    case PROCESSING = "PROCESSING";
+    case INTERVIEWING = "INTERVIEWING";
     case HIRED = "HIRED";
     case ARCHIVED = "ARCHIVED";
     case BLACKLISTED = "BLACKLISTED";
