2eeda14036e166b627f18ba47da53bc816e28651
Ha Quanq <quangha0120010@gmail.com>
2025-12-01 23:54:19 +0700

    Server: add EducationLevelResourceCollection

---
+++ apps/server/app/Modules/EducationLevel/Resources/EducationLevelResourceCollection.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Modules\EducationLevel\Resources;
+
+use App\Abstracts\BaseResourceCollection;
+
+class EducationLevelResourceCollection extends BaseResourceCollection
+{
+    public $collects = EducationLevelResource::class;
+}
