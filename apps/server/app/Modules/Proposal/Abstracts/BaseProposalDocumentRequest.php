30c54b10e0bcf4a51b98e52ef689ec66b5b982c0
Ha Quanq <quangha0120010@gmail.com>
2025-12-03 06:00:11 +0700

    server: add base ProposalDocument validation docs

--- apps/server/app/Modules/Proposal/Abstracts/BaseProposalDocumentRequest.php
+++ apps/server/app/Modules/Proposal/Abstracts/BaseProposalDocumentRequest.php
@@ -15,8 +15,20 @@ abstract class BaseProposalDocumentRequest extends BaseFormRequest
     public function rules(): array
     {
         return [
-            "candidate_id" => ["required", "integer", "exists:candidate,id"],
+            /**
+             * Id of Proposal where document will be attached
+             * @example 1
+             */
+            "proposal_id" => ["required", "integer", "exists:proposal,id"],
+            /**
+             * Document file (.pdf, .docx, .doc).
+             * Max: 5MB
+             */
             "document" => ["required", FileRule::types(["pdf", "docx", "doc"])->max(5 * 1024)],
+            /**
+             * Description
+             * @example "Requirements"
+             */
             "description" => ["string", "max:500"],
         ];
     }
