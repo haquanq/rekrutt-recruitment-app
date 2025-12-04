053077ce90f0638c369feaa31a7d24316880e9f8
Ha Quanq <quangha0120010@gmail.com>
2025-12-04 17:10:55 +0700

    Server: add CandidateExistsWithStatusRule

---
+++ apps/server/app/Modules/Candidate/Rules/CandidateExistsWithStatusRule.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Modules\Candidate\Rules;
+
+use App\Modules\Candidate\Enums\CandidateStatus;
+use App\Modules\Candidate\Models\Candidate;
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class CandidateExistsWithStatusRule implements ValidationRule
+{
+    public function __construct(protected CandidateStatus $requiredStatus, protected ?Candidate $candidate = null) {}
+
+    public function validate(string $attribute, mixed $id, Closure $fail): void
+    {
+        if (!$this->candidate) {
+            $this->candidate = Candidate::find($id);
+        }
+
+        if (!$this->candidate) {
+            $fail("Candidate does not exist");
+            return;
+        }
+
+        if ($this->candidate->status !== $this->requiredStatus) {
+            $fail("Candidate must have status: " . $this->requiredStatus->value);
+            return;
+        }
+    }
+}
