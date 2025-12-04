8731759f2cb11cdc238b3982d13146f8fddcd13d
Ha Quanq <quangha0120010@gmail.com>
2025-12-04 23:27:13 +0700

    Server: add CandidateExperienceResourceCollection

---
+++ apps/server/app/Modules/Candidate/Resources/CandidateExperienceResourceCollection.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Modules\Candidate\Resources;
+
+use App\Abstracts\BaseResourceCollection;
+
+class CandidateExperienceResourceCollection extends BaseResourceCollection
+{
+    public $collects = CandidateExperienceResource::class;
+}
