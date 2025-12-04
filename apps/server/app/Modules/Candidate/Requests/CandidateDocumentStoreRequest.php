e40eafcf7ed5e90d273a1a89c2a8aee8218af3aa
Ha Quanq <quangha0120010@gmail.com>
2025-12-05 01:37:16 +0700

    Server: update CandidateDocumentStoreRequest rules

--- apps/server/app/Modules/Candidate/Requests/CandidateDocumentStoreRequest.php
+++ apps/server/app/Modules/Candidate/Requests/CandidateDocumentStoreRequest.php
@@ -3,11 +3,15 @@
 namespace App\Modules\Candidate\Requests;
 
 use App\Modules\Candidate\Abstracts\BaseCandidateDocumentRequest;
+use App\Modules\Candidate\Enums\CandidateStatus;
+use App\Modules\Candidate\Rules\CandidateExistsWithStatusRule;
 
 class CandidateDocumentStoreRequest extends BaseCandidateDocumentRequest
 {
     public function rules(): array
     {
-        return array_merge(parent::rules(), []);
+        return parent::rules() + [
+            "candidate_id" => ["required", "integer", new CandidateExistsWithStatusRule(CandidateStatus::PENDING)],
+        ];
     }
 }
