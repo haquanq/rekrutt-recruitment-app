7f478c13fa596f85e87aa2d66c1810688fcc2ada
Ha Quanq <quangha0120010@gmail.com>
2025-12-05 02:17:28 +0700

    Server: add CandidateExperience endpoints docs

--- apps/server/app/Modules/Candidate/Controllers/CandidateExperienceController.php
+++ apps/server/app/Modules/Candidate/Controllers/CandidateExperienceController.php
@@ -10,6 +10,7 @@ use App\Modules\Candidate\Requests\CandidateExperienceStoreRequest;
 use App\Modules\Candidate\Requests\CandidateExperienceUpdateRequest;
 use App\Modules\Candidate\Resources\CandidateExperienceResource;
 use App\Modules\Candidate\Resources\CandidateExperienceResourceCollection;
+use Dedoc\Scramble\Attributes\QueryParameter;
 use Illuminate\Support\Facades\Gate;
 use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\QueryBuilder;
@@ -17,6 +18,48 @@ use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
 
 class CandidateExperienceController extends BaseController
 {
+    /**
+     * Find all candidate experiences
+     *
+     * Return a list of candidate experiences. Allows pagination, relations and filters query.
+     *
+     * Authorization rules:
+     * - User with roles: any.
+     */
+    #[
+        QueryParameter(
+            name: "page[number]",
+            type: "integer",
+            description: "Current page number (default: 1)",
+            example: 1,
+        ),
+    ]
+    #[
+        QueryParameter(
+            name: "page[size]",
+            type: "integer",
+            description: "Size of current page (default: 15, max: 100)",
+            example: 15,
+        ),
+    ]
+    #[
+        QueryParameter(
+            name: "include",
+            type: "string",
+            description: "Include nested relations </br>" .
+                " Allow relations: candidate </br>" .
+                "Example: include=candidate",
+        ),
+    ]
+    #[
+        QueryParameter(
+            name: "filter[*]",
+            type: "string",
+            description: "Filter by fields </br>" .
+                "Allow fields: candidateId </br>" .
+                "Example: filter[candidateId]=32",
+        ),
+    ]
     public function index()
     {
         Gate::authorize("viewAny", CandidateExperience::class);
@@ -33,6 +76,23 @@ class CandidateExperienceController extends BaseController
         return CandidateExperienceResourceCollection::make($candidateExperiences);
     }
 
+    /**
+     * Find candidate experience by Id
+     *
+     * Return a unique candidate experience. Allows relations query.
+     *
+     * Authorization rules:
+     * - User with roles: any.
+     */
+    #[
+        QueryParameter(
+            name: "include",
+            type: "string",
+            description: "Include nested relations </br>" .
+                " Allow relations: candidate </br>" .
+                "Example: include=candidate",
+        ),
+    ]
     public function show(int $id)
     {
         Gate::authorize("view", CandidateExperience::class);
@@ -44,12 +104,32 @@ class CandidateExperienceController extends BaseController
         return $this->okResponse(new CandidateExperienceResource($candidateExperience));
     }
 
+    /**
+     * Create candidate experience
+     *
+     * Return created candidate experience.
+     *
+     * Authorization rules:
+     * - User with roles: RECRUITER, HIRING_MANAGER.
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
     public function store(CandidateExperienceStoreRequest $request)
     {
         $createdCandidateExperience = CandidateExperience::create($request->validated());
         return $this->createdResponse(new CandidateExperienceResource($createdCandidateExperience));
     }
 
+    /**
+     * Update candidate experience
+     *
+     * Return no content.
+     *
+     * Authorization rules:
+     * - User with roles: RECRUITER, HIRING_MANAGER.
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
     public function update(CandidateExperienceUpdateRequest $request)
     {
         $candidateStatus = $request->candidateExperience->candidate->status;
@@ -62,6 +142,16 @@ class CandidateExperienceController extends BaseController
         return $this->noContentResponse();
     }
 
+    /**
+     * Delete candidate experience
+     *
+     * Permanently delete candidate experience. Return no content.
+     *
+     * Authorization rules:
+     * - User with roles: RECRUITER, HIRING_MANAGER.
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
     public function destroy(CandidateExperienceDestroyRequest $request)
     {
         $candidateStatus = $request->candidateExperience->candidate->status;
