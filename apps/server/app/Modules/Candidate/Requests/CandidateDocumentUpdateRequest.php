e820717a60513f6e7bf6486d71cb16a00ae8c8e9
Ha Quanq <quangha0120010@gmail.com>
2025-12-05 01:38:38 +0700

    Server: update CandidateDocumentUpdateRequest rules

--- apps/server/app/Modules/Candidate/Requests/CandidateDocumentUpdateRequest.php
+++ apps/server/app/Modules/Candidate/Requests/CandidateDocumentUpdateRequest.php
@@ -9,7 +9,7 @@ class CandidateDocumentUpdateRequest extends BaseCandidateDocumentRequest
     public function rules(): array
     {
         $rules = parent::rules();
-        echo json_encode($rules["note"]);
-        return ["note" => $rules["note"]];
+        unset($rules["document"]);
+        return $rules;
     }
 }
