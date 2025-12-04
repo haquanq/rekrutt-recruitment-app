bfb3364e25ebbb24250f92729fb217ea05e80db7
Ha Quanq <quangha0120010@gmail.com>
2025-12-05 02:51:55 +0700

    Server: remove debug loggers

--- apps/server/app/Modules/Proposal/Requests/ProposalDestroyRequest.php
+++ apps/server/app/Modules/Proposal/Requests/ProposalDestroyRequest.php
@@ -3,11 +3,8 @@
 namespace App\Modules\Proposal\Requests;
 
 use App\Modules\Proposal\Abstracts\BaseProposalRequest;
-use App\Modules\Proposal\Enums\ProposalStatus;
 use App\Modules\Proposal\Models\Proposal;
-use App\Modules\Proposal\Rules\ProposalStatusTransitionsFromRule;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\Log;
 
 class ProposalDestroyRequest extends BaseProposalRequest
 {
@@ -15,20 +12,17 @@ class ProposalDestroyRequest extends BaseProposalRequest
 
     public function rules(): array
     {
-        Log::info("rules");
         return [];
     }
 
     public function authorize(): bool
     {
-        Log::info("authorize");
         Gate::authorize("delete", $this->proposal);
         return true;
     }
 
     public function prepareForValidation(): void
     {
-        Log::info("prepare");
         parent::prepareForValidation();
         $this->proposal = Proposal::findOrFail($this->route("id"));
     }
