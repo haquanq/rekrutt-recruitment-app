ef644ed68f83b03e74cc44149304641fa43b581a
Ha Quanq <quangha0120010@gmail.com>
2025-12-05 13:24:35 +0700

    Server: update RecruitmentStatus enum

--- apps/server/app/Modules/Recruitment/Enums/RecruitmentStatus.php
+++ apps/server/app/Modules/Recruitment/Enums/RecruitmentStatus.php
@@ -5,6 +5,7 @@ namespace App\Modules\Recruitment\Enums;
 enum RecruitmentStatus: string
 {
     case DRAFT = "DRAFT";
+    case SCHEDULED = "SCHEDULED";
     case PUBLISHED = "PUBLISHED";
     case CLOSED = "CLOSED";
     case COMPLETED = "COMPLETED";
@@ -13,6 +14,7 @@ enum RecruitmentStatus: string
     {
         return match ($this) {
             self::DRAFT => "Recruitment is draft.",
+            self::SCHEDULED => "Recruitment is scheduled.",
             self::PUBLISHED => "Recruitment is published.",
             self::CLOSED => "Recruitment is closed.",
             self::COMPLETED => "Recruitment is completed.",
