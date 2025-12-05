a9d5e56c55b1fc1fbdc651e331e3542ff7ddd732
Ha Quanq <quangha0120010@gmail.com>
2025-12-05 12:38:59 +0700

    Server: update RecruitmentStoreRequest

--- apps/server/app/Modules/Recruitment/Requests/RecruitmentStoreRequest.php
+++ apps/server/app/Modules/Recruitment/Requests/RecruitmentStoreRequest.php
@@ -45,6 +45,8 @@ class RecruitmentStoreRequest extends BaseRecruitmentRequest
 
     public function prepareForValidation(): void
     {
+        parent::prepareForValidation();
+
         $this->merge([
             "created_by_user_id" => Auth::user()->id,
             "status" => RecruitmentStatus::DRAFT->value,
