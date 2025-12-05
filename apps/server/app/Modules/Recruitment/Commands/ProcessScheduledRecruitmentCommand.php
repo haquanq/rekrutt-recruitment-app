dc276c526ba6b3c48852b88f8b54e57c8514bf7b
Ha Quanq <quangha0120010@gmail.com>
2025-12-05 13:49:43 +0700

    Server: add command to process scheduled Recruitment

---
+++ apps/server/app/Modules/Recruitment/Commands/ProcessScheduledRecruitmentCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Modules\Recruitment\Commands;
+
+use App\Modules\Recruitment\Enums\RecruitmentStatus;
+use App\Modules\Recruitment\Models\Recruitment;
+use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
+
+class ProcessScheduledRecruitmentCommand extends Command
+{
+    protected $signature = "recruitment:process-sheduled";
+    protected $description = "Process scheduled recruitment statuses.";
+
+    public function handle(): void
+    {
+        Recruitment::where("status", RecruitmentStatus::DRAFT->value)
+            ->where("scheduled_start_at", "<=", Carbon::now())
+            ->where("status", "=", RecruitmentStatus::DRAFT->value)
+            ->update(["status" => RecruitmentStatus::PUBLISHED->value, "published_at" => Carbon::now()]);
+
+        Recruitment::where("status", RecruitmentStatus::DRAFT->value)
+            ->where("scheduled_end_at", "<=", Carbon::now())
+            ->update(["status" => RecruitmentStatus::CLOSED->value, "closed_at" => Carbon::now()]);
+    }
+}
