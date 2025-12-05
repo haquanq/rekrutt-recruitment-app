3e273281bbf9de9e6bd9112d044db44e292bde43
Ha Quanq <quangha0120010@gmail.com>
2025-12-05 13:55:02 +0700

    Server: add RecruitmentApplication enum casts

--- apps/server/app/Modules/Recruitment/Models/RecruitmentApplication.php
+++ apps/server/app/Modules/Recruitment/Models/RecruitmentApplication.php
@@ -5,12 +5,19 @@ namespace App\Modules\Recruitment\Models;
 use App\Abstracts\BaseModel;
 use App\Modules\Candidate\Models\Candidate;
 use App\Modules\Interview\Models\Interview;
+use App\Modules\Recruitment\Enums\RecruitmentApplicationPriority;
+use App\Modules\Recruitment\Enums\RecruitmentApplicationStatus;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class RecruitmentApplication extends BaseModel
 {
     protected $guarded = ["id", "created_at", "updated_at"];
+
+    protected $casts = [
+        "status" => RecruitmentApplicationStatus::class,
+        "priority" => RecruitmentApplicationPriority::class,
+    ];
 
     public function recruitment(): BelongsTo
     {
