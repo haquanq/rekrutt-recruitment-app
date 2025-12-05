9b2bda23210c84dd5edb3d98b9b39794392a0adc
Ha Quanq <quangha0120010@gmail.com>
2025-12-05 13:43:54 +0700

    Server: update UserSeeder

--- apps/server/database/seeders/UserSeeder.php
+++ apps/server/database/seeders/UserSeeder.php
@@ -3,36 +3,60 @@
 namespace Database\Seeders;
 
 use App\Modules\Auth\Enums\UserRole;
-use Carbon\Carbon;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        $recruiterIds = [41, 41, 41, 41, 41, 43, 43, 43];
+        $users = [];
 
-        foreach ($recruiterIds as $id) {
+        $recruiterPositionIds = [41, 41, 41, 41, 43, 43, 43];
+
+        UserFactory::new()->create([
+            "email" => "recruiter@gmail.com",
+            "position_id" => 41,
+            "role" => UserRole::RECRUITER,
+        ]);
+
+        foreach ($recruiterPositionIds as $id) {
             UserFactory::new()->create([
                 "position_id" => $id,
                 "role" => UserRole::RECRUITER,
             ]);
         }
 
-        $managerIds = [8, 10, 11, 12, 25, 29, 31, 34, 37, 39, 42, 44];
+        $managerPositionIds = [10, 11, 12, 25, 29, 31, 34, 37, 39, 42, 44];
 
-        foreach ($managerIds as $id) {
+        UserFactory::new()->create([
+            "email" => "manager@gmail.com",
+            "role" => UserRole::MANAGER,
+            "position_id" => 8,
+        ]);
+
+        UserFactory::new()->create([
+            "email" => "hiring.manager@gmail.com",
+            "role" => UserRole::HIRING_MANAGER,
+            "position_id" => 41,
+        ]);
+
+        foreach ($managerPositionIds as $id) {
             UserFactory::new()->create([
                 "position_id" => $id,
                 "role" => UserRole::MANAGER,
             ]);
         }
 
-        $executiveIds = [9, 14, 20, 28, 30, 36, 45, 50];
+        $executivePositionIds = [14, 20, 28, 30, 36, 45, 50];
 
-        foreach ($executiveIds as $id) {
+        UserFactory::new()->create([
+            "email" => "executive@gmail.com",
+            "role" => UserRole::EXECUTIVE,
+            "position_id" => 9,
+        ]);
+
+        foreach ($executivePositionIds as $id) {
             UserFactory::new()->create([
                 "position_id" => $id,
                 "role" => UserRole::EXECUTIVE,
@@ -43,12 +67,8 @@ class UserSeeder extends Seeder
             "first_name" => "Ha",
             "last_name" => "Quang",
             "email" => "admin@gmail.com",
-            "phone_number" => "0123456789",
             "username" => "haquanq",
             "role" => UserRole::ADMIN,
-            "password" => Hash::make("12345678"),
-            "created_at" => Carbon::now(),
-            "updated_at" => Carbon::now(),
             "position_id" => 44,
         ]);
     }
