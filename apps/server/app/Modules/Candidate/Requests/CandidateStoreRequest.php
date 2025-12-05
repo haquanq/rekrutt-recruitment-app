1d0ccaa97674aa65ba746d66a0b0d489b3cac3ba
Ha Quanq <quangha0120010@gmail.com>
2025-12-05 18:20:27 +0700

    Server: update CandidateStoreRequest

--- apps/server/app/Modules/Candidate/Requests/CandidateStoreRequest.php
+++ apps/server/app/Modules/Candidate/Requests/CandidateStoreRequest.php
@@ -3,11 +3,24 @@
 namespace App\Modules\Candidate\Requests;
 
 use App\Modules\Candidate\Abstracts\BaseCandidateRequest;
+use App\Modules\Candidate\Enums\CandidateStatus;
 use App\Modules\Candidate\Models\Candidate;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\Rule;
 
 class CandidateStoreRequest extends BaseCandidateRequest
 {
+    public function rules(): array
+    {
+        return [
+            /**
+             * Initial status (generated automatically)
+             * @ignoreParam
+             */
+            "status" => ["required", Rule::enum(CandidateStatus::class)->only(CandidateStatus::PENDING)],
+        ];
+    }
+
     public function authorize(): bool
     {
         Gate::authorize("create", Candidate::class);
