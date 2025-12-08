9c6857f7f0edce1221b0482909c05716cb692277
Ha Quanq <quangha0120010@gmail.com>
2025-12-08 07:28:14 +0700

    Server: update RecruitmentApplication DB schema

--- apps/server/database/migrations/2025_11_12_100019_create_recruitment_application_table.php
+++ apps/server/database/migrations/2025_11_12_100019_create_recruitment_application_table.php
@@ -12,7 +12,23 @@ return new class extends Migration {
     {
         Schema::create("recruitment_application", function (Blueprint $table) {
             $table->bigInteger("id")->generatedAs()->always();
-            $table->timestampTZ("completed_at")->nullable();
+            $table->integer("number_of_interviews")->nullable();
+
+            $table->timestampTZ("withdrawn_at")->nullable();
+            $table->string("withdrawn_reason")->nullable();
+
+            $table->timestampTZ("discarded_at")->nullable();
+            $table->string("discard_reason", 300)->nullable();
+            $table
+                ->foreignId("discarded_by_user_id")
+                ->nullable()
+                ->constrained(table: "user", indexName: "fk_recruitment_application__discarded_by_user");
+
+            $table->timestampTZ("offer_started_at")->nullable();
+            $table->timestampTZ("offer_expired_at")->nullable();
+            $table->timestampTZ("offer_responded_at")->nullable();
+            $table->string("offer_rejected_reason", 300)->nullable();
+
             $table->timestampsTZ();
 
             $table
