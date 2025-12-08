6b73a6605b237b51ff8fbd951220d8df3e97c2ac
Ha Quanq <quangha0120010@gmail.com>
2025-12-08 10:46:23 +0700

    Server: add RecruitmentApplication scope

--- apps/server/app/Modules/Recruitment/Models/RecruitmentApplication.php
+++ apps/server/app/Modules/Recruitment/Models/RecruitmentApplication.php
@@ -8,6 +8,7 @@ use App\Modules\Candidate\Models\Candidate;
 use App\Modules\Interview\Models\Interview;
 use App\Modules\Recruitment\Enums\RecruitmentApplicationPriority;
 use App\Modules\Recruitment\Enums\RecruitmentApplicationStatus;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
@@ -19,6 +20,16 @@ class RecruitmentApplication extends BaseModel
         "status" => RecruitmentApplicationStatus::class,
         "priority" => RecruitmentApplicationPriority::class,
     ];
+
+    public function scopeCompleted(Builder $query): void
+    {
+        $query->whereIn("status", [
+            RecruitmentApplicationStatus::WITHDRAWN->value,
+            RecruitmentApplicationStatus::DISCARDED->value,
+            RecruitmentApplicationStatus::OFFER_ACCEPTED->value,
+            RecruitmentApplicationStatus::OFFER_REJECTED->value,
+        ]);
+    }
 
     public function recruitment(): BelongsTo
     {
