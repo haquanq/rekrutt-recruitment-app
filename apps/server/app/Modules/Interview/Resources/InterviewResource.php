eeaf4e29b2c9d73062c586d70f66436c98c87b4e
Ha Quanq <quangha0120010@gmail.com>
2025-12-08 10:11:24 +0700

    Server: update InterviewResource

--- apps/server/app/Modules/Interview/Resources/InterviewResource.php
+++ apps/server/app/Modules/Interview/Resources/InterviewResource.php
@@ -16,20 +16,23 @@ class InterviewResource extends JsonResource
             "id" => $this->id,
             "title" => $this->title,
             "description" => $this->description,
-            "scheduled_start_at" => $this->scheduled_start_at,
-            "scheduled_end_at" => $this->scheduled_end_at,
             "started_at" => $this->started_at,
             "ended_at" => $this->ended_at,
-            "cancelled_at" => $this->cancelled_at,
-            "cancellation_note" => $this->cancellation_note,
+            "completed_at" => $this->completed_at,
             "status" => $this->status,
+
+            "method" => InterviewMethodResource::make($this->whenLoaded("method")),
+            "rating_scale" => RatingScaleResource::make($this->whenLoaded("ratingScale")),
+
+            "cancelled_at" => $this->cancelled_at,
+            "cancelled_reason" => $this->cancelled_reason,
+            "cancelled_by" => UserResource::make($this->whenLoaded("cancelledBy")),
+
             "created_at" => $this->created_at,
             "updated_at" => $this->updated_at,
+            "created_by" => UserResource::make($this->whenLoaded("createdBy")),
+
             "application" => RecruitmentApplicationResource::make($this->whenLoaded("application")),
-            "method" => InterviewMethodResource::make($this->whenLoaded("method")),
-            "createdBy" => UserResource::make($this->whenLoaded("createdBy")),
-            "cancelledBy" => UserResource::make($this->whenLoaded("cancelledBy")),
-            "ratingScale" => RatingScaleResource::make($this->whenLoaded("ratingScale")),
             "evaluations" => InterviewEvaluationResource::make($this->whenLoaded("evaluations")),
             "participants" => UserResource::make($this->whenLoaded("interviewers")),
         ];
