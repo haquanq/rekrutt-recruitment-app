771847d1c5c41a6ee3aa4aa6f7a6c9953e91d40b
Ha Quanq <quangha0120010@gmail.com>
2025-12-08 15:42:19 +0700

    Server: update Interview create request

--- apps/server/app/Modules/Interview/Requests/InterviewStoreRequest.php
+++ apps/server/app/Modules/Interview/Requests/InterviewStoreRequest.php
@@ -5,9 +5,13 @@ namespace App\Modules\Interview\Requests;
 use App\Modules\Interview\Abstracts\BaseInterviewRequest;
 use App\Modules\Interview\Enums\InterviewStatus;
 use App\Modules\Interview\Models\Interview;
+use App\Modules\Recruitment\Enums\RecruitmentApplicationStatus;
+use App\Modules\Recruitment\Models\RecruitmentApplication;
+use App\Modules\Recruitment\Rules\RecruitmentApplicationExistsWithStatusRule;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Validation\Rule;
+use Illuminate\Validation\Validator;
 
 class InterviewStoreRequest extends BaseInterviewRequest
 {
@@ -30,9 +34,31 @@ class InterviewStoreRequest extends BaseInterviewRequest
                  * Id of RecruitmentApplication
                  * @example 1
                  */
-                "recruitment_applicant_id" => ["required", "integer:strict"],
+                "recruitment_application_id" => [
+                    "required",
+                    "integer:strict",
+                    new RecruitmentApplicationExistsWithStatusRule(RecruitmentApplicationStatus::INTERVIEW_PLANNING),
+                ],
             ],
         ];
+    }
+
+    public function withValidator(Validator $validator): void
+    {
+        $validator->after(function (Validator $validator) {
+            $recruitmentApplication = RecruitmentApplication::withCount("interviews")->findOrFail(
+                $this->input("recruitment_application_id"),
+            );
+
+            if ($recruitmentApplication->interviews_count >= $recruitmentApplication->number_of_interviews) {
+                $validator
+                    ->errors()
+                    ->add(
+                        "recruitment_application_id",
+                        "Recruitment application has reached the maximum number of interviews.",
+                    );
+            }
+        });
     }
 
     public function authorize(): bool
