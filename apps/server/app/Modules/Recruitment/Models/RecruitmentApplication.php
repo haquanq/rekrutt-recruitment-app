4416f9288804b8b61f80e79359ee1bf4345b75fe
Ha Quanq <quangha0120010@gmail.com>
2025-12-08 07:50:57 +0700

    Server: add RecruitmentApplication relation

--- apps/server/app/Modules/Recruitment/Models/RecruitmentApplication.php
+++ apps/server/app/Modules/Recruitment/Models/RecruitmentApplication.php
@@ -3,6 +3,7 @@
 namespace App\Modules\Recruitment\Models;
 
 use App\Abstracts\BaseModel;
+use App\Modules\Auth\Models\User;
 use App\Modules\Candidate\Models\Candidate;
 use App\Modules\Interview\Models\Interview;
 use App\Modules\Recruitment\Enums\RecruitmentApplicationPriority;
@@ -27,6 +28,11 @@ class RecruitmentApplication extends BaseModel
     public function candidate(): BelongsTo
     {
         return $this->belongsTo(Candidate::class);
+    }
+
+    public function discardedBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, "discarded_by_user_id", "id");
     }
 
     public function interviews(): HasMany
