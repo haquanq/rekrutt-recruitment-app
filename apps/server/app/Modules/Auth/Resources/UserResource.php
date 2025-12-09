ccaddb318dcf7dd4aee8768077dcd69b73d1eb02
Ha Quanq <quangha0120010@gmail.com>
2025-12-09 21:10:30 +0700

    Server: limit User fields to only ADMIN

--- apps/server/app/Modules/Auth/Resources/UserResource.php
+++ apps/server/app/Modules/Auth/Resources/UserResource.php
@@ -2,14 +2,18 @@
 
 namespace App\Modules\Auth\Resources;
 
+use App\Modules\Auth\Enums\UserRole;
 use App\Modules\Position\Resources\PositionResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 
 class UserResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
+        $userIsAdmin = Auth::user()->role === UserRole::ADMIN;
+
         return [
             "id" => $this->id,
             "first_name" => $this->first_name,
@@ -18,9 +22,9 @@ class UserResource extends JsonResource
             "username" => $this->username,
             "phone_number" => $this->phone_number,
             "retired_at" => $this->retired_at,
-            "suspension_started_at" => $this->suspension_started_at,
-            "suspension_ended_at" => $this->suspension_ended_at,
-            "suspension_note" => $this->suspension_note,
+            "suspension_started_at" => $this->when($userIsAdmin, $this->suspension_started_at),
+            "suspension_ended_at" => $this->when($userIsAdmin, $this->suspension_ended_at),
+            "suspension_note" => $this->when($userIsAdmin, $this->suspension_note),
             "role" => $this->role,
             "status" => $this->status,
             "created_at" => $this->created_at,
