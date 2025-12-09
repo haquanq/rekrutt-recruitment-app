553c85dc41f7a7b96ef12656441f8bc415311111
Ha Quanq <quangha0120010@gmail.com>
2025-12-09 21:12:38 +0700

    Server: refactor Position requests

--- apps/server/app/Modules/Position/Requests/PositionStoreRequest.php
+++ apps/server/app/Modules/Position/Requests/PositionStoreRequest.php
@@ -4,10 +4,4 @@ namespace App\Modules\Position\Requests;
 
 use App\Modules\Position\Abstracts\BasePositionRequest;
 
-class PositionStoreRequest extends BasePositionRequest
-{
-    public function rules(): array
-    {
-        return array_merge(parent::rules(), []);
-    }
-}
+class PositionStoreRequest extends BasePositionRequest {}
--- apps/server/app/Modules/Position/Requests/PositionUpdateRequest.php
+++ apps/server/app/Modules/Position/Requests/PositionUpdateRequest.php
@@ -4,10 +4,4 @@ namespace App\Modules\Position\Requests;
 
 use App\Modules\Position\Abstracts\BasePositionRequest;
 
-class PositionUpdateRequest extends BasePositionRequest
-{
-    public function rules(): array
-    {
-        return array_merge(parent::rules(), []);
-    }
-}
+class PositionUpdateRequest extends BasePositionRequest {}
