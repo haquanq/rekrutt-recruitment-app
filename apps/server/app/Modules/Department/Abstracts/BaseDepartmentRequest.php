cee39b73f43c6ecfd872841469e632cb0a606bdc
Ha Quanq <quangha0120010@gmail.com>
2025-12-10 20:34:56 +0700

    Server: add Department method to resolve queried model

--- apps/server/app/Modules/Department/Abstracts/BaseDepartmentRequest.php
+++ apps/server/app/Modules/Department/Abstracts/BaseDepartmentRequest.php
@@ -3,13 +3,11 @@
 namespace App\Modules\Department\Abstracts;
 
 use App\Abstracts\BaseFormRequest;
+use App\Modules\Department\Models\Department;
 
 abstract class BaseDepartmentRequest extends BaseFormRequest
 {
-    public function authorize(): bool
-    {
-        return true;
-    }
+    protected ?Department $department = null;
 
     public function rules(): array
     {
@@ -25,5 +23,14 @@ abstract class BaseDepartmentRequest extends BaseFormRequest
              */
             "description" => ["nullable", "string", "max:500"],
         ];
+    }
+
+    public function getDepartment(string $param = null): ?Department
+    {
+        if ($this->department === null) {
+            $this->department = Department::findOrFail($this->route($param ?? "id"));
+        }
+
+        return $this->department;
     }
 }
