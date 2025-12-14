113be6e70956f4a6436e3429789a4d8e275aad43
Ha Quanq <quangha0120010@gmail.com>
2025-12-15 02:13:48 +0700

    Server: remove non-exist route

--- apps/server/routes/api.php
+++ apps/server/routes/api.php
@@ -26,7 +26,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix("auth")->group(function () {
     Route::controller(AuthController::class)->group(function () {
         Route::post("login", "login")->name("login");
-        Route::post("refresh", "refresh");
     });
 
     Route::middleware("auth:sanctum")
