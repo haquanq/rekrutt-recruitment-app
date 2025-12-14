7fdcf1e6e685663263ba54b1e5883831de630ee8
Ha Quanq <quangha0120010@gmail.com>
2025-12-14 14:15:56 +0700

    Server: update ProposalDocument database schema

--- apps/server/app/Modules/Proposal/Resources/ProposalDocumentResource.php
+++ apps/server/app/Modules/Proposal/Resources/ProposalDocumentResource.php
@@ -4,6 +4,7 @@ namespace App\Modules\Proposal\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\URL;
 
 class ProposalDocumentResource extends JsonResource
@@ -12,15 +13,14 @@ class ProposalDocumentResource extends JsonResource
     {
         return [
             "id" => $this->id,
-            "file_id" => $this->file_id,
+            "file_url" => URL::to("/") . Storage::url($this->file_path),
             "file_name" => $this->file_name,
-            "file_url" => URL::to("/") . $this->file_url,
             "file_exension" => $this->file_extension,
             "mime_type" => $this->mime_type,
-            "note" => $this->note,
+            "notes" => $this->notes,
             "created_at" => $this->created_at,
             "updated_at" => $this->updated_at,
-            "candidate" => new ProposalResource($this->whenLoaded("candidate")),
+            "proposal" => ProposalResource::make($this->whenLoaded("proposal")),
         ];
     }
 }
--- apps/server/database/migrations/2025_11_12_100017_create_proposal_document_table.php
+++ apps/server/database/migrations/2025_11_12_100017_create_proposal_document_table.php
@@ -10,20 +10,17 @@ return new class extends Migration {
     {
         Schema::create("proposal_document", function (Blueprint $table) {
             $table->bigInteger("id")->generatedAs()->always();
-            $table->string("file_id", 256);
+            $table->text("file_path");
             $table->string("file_name", 256);
-            $table->string("file_url", 500)->nullable();
             $table->string("file_extension", 50)->nullable();
             $table->string("mime_type", 300);
-            $table->string("description", 500)->nullable();
+            $table->string("notes", 500)->nullable();
             $table->timestampsTZ();
 
             $table
                 ->foreignId("proposal_id")
                 ->constrained(table: "proposal", indexName: "fk_proposal_document__proposal")
                 ->onDelete("cascade");
-
-            $table->unique(columns: ["file_id", "proposal_id"], name: "uq_proposal_document__file_id_proposal_id");
         });
 
         DB::statement("ALTER TABLE public.proposal_document ADD CONSTRAINT pk_proposal_document PRIMARY KEY (id)");
