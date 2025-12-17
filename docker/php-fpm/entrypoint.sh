019e5d80fbe30891db31bac490340e6e79f6a8d4
Ha Quanq <quangha0120010@gmail.com>
2025-12-17 07:01:12 +0700

    Docker: add configs for production

---
+++ docker/php-fpm/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+cat > .env << EOF
+APP_KEY=generated
+APP_ENV=${APP_ENV}
+APP_DEBUG=${APP_DEBUG}
+APP_NAME=${APP_NAME}
+APP_URL=${APP_URL}
+
+DB_HOST=db
+DB_PORT=5432
+DB_DATABASE=recruitment_db
+DB_USERNAME=${DB_USERNAME}
+DB_PASSWORD=${DB_PASSWORD}
+DB_CONNECTION=pgsql
+EOF
+
+php artisan key:generate
+
+exec "$@"
